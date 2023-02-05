f4605f7a3f8b70cd8b6ab99331c968164580d879
RBaptista <riniltton11@gmail.com>
2023-02-05 19:10:19 -0100

    ml kit

---
+++ app/src/main/java/cv/edu/unicv/facefinder/Helpers/Box.java
@@ -0,0 +1,10 @@
+package cv.edu.unicv.facefinder.Helpers;
+
+import android.graphics.Rect;
+
+public class Box {
+    public Rect rect;
+    public Box(Rect rect) {
+        this.rect = rect;
+    }
+}
--- app/src/main/java/cv/edu/unicv/facefinder/MainActivity.java
+++ app/src/main/java/cv/edu/unicv/facefinder/MainActivity.java
@@ -2,13 +2,30 @@ package cv.edu.unicv.facefinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.FaceDetector;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import cv.edu.unicv.facefinder.Helpers.ImageHelperActivity;
+import cv.edu.unicv.facefinder.funcs.FaceDetectionActivity;
+import cv.edu.unicv.facefinder.funcs.ImageClassifierActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void onGoToImageClassification(View view){
+        Intent intent = new Intent(this, ImageClassifierActivity.class);
+        startActivity(intent);
+    }
+
+    public void onGoToFaceDetector(View view){
+        Intent intent = new Intent(this, FaceDetectionActivity.class);
+        startActivity(intent);
+    }
+
 }
