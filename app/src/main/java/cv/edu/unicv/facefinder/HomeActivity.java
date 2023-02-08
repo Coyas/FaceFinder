5339b2b021f29d85f0a8bedaeece14de97978ef4
Ailton Duarte <adidas.coyas@gmail.com>
2023-02-08 05:25:29 -0100

    bottom menu with fragment & bugs

---
+++ app/src/main/java/cv/edu/unicv/facefinder/HomeActivity.java
@@ -0,0 +1,46 @@
+package cv.edu.unicv.facefinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import cv.edu.unicv.facefinder.databinding.ActivityHomeBinding;
+
+public class HomeActivity extends AppCompatActivity {
+
+    ActivityHomeBinding binding;
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.fd:
+                    replaceFragment(new FDFragment());
+                    break;
+                case R.id.config:
+                    replaceFragment(new ConfFragment());
+                    break;
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+    }
+}
--- app/src/main/java/cv/edu/unicv/facefinder/LoginActivity.java
+++ app/src/main/java/cv/edu/unicv/facefinder/LoginActivity.java
@@ -2,13 +2,25 @@ package cv.edu.unicv.facefinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent  it = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(it);
+            }
+        });
     }
 }
